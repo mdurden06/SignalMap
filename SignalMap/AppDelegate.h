4643a56699fedc5b85d4144ceafd53cbb6993f8c
Ethan Laur <phyrrus9@users-macbook-pro.local>
2015-10-15 15:02:53 -0700

    Initial Commit

---
+++ SignalMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SignalMap
+//
+//  Created by Ethan Laur on 10/15/15.
+//  Copyright © 2015 Ethan Laur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SignalMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SignalMap
+//
+//  Created by Ethan Laur on 10/15/15.
+//  Copyright © 2015 Ethan Laur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
